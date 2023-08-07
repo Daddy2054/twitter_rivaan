a3278f44f1f5ccba48cba4e433beec7181f89de2
Jean <acalto2@gmail.com>
2023-08-07 20:41:02 +0200

    Setting up Flutter Project

--- lib/constants/appwrite_constants.dart
+++ lib/constants/appwrite_constants.dart
@@ -1,3 +1,6 @@
 class AppwriteConstants {
   static const String databaseId = '64d130dce008a49555c0';
+  static const String projectId = '64d12bef09e66836040f';
+  static const String endPoint = 'https://cloud.appwrite.io/v1';
+
 }
